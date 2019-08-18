cbd55a7b90e952158af89777074cf15b49ad4bfa
Do Thanh Nhat <dothanhnhat91@gmail.com>
2019-08-18 12:03:24 +0700

    initial commit

---
+++ catalog/language/en-gb/extension/payment/cod.php
@@ -0,0 +1,3 @@
+<?php
+// Text
+$_['text_title'] = 'Trả tiền khi nhận hàng';
---
+++ catalog/language/en-gb/extension/shipping/free.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title']       = 'Phương thức vận chuyển';
+$_['text_description'] = 'Miễn phí vận chuyển';
