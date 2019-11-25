f38891568d2706c6ceb88b7f7cdb033421aebdc5
Do Thanh Nhat <dothanhnhat91@gmail.com>
2019-11-25 20:29:55 +0700

    change text

--- quanly/language/vi-vn/common/footer.php
+++ quanly/language/vi-vn/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer']  = 'Bản quyền thuộc về Market &copy; ' . date('Y') . ' All Rights Reserved.';
+$_['text_footer']  = 'Bản quyền thuộc về <a href="https://ketnoicho.com">ketnoicho.com</a> &copy; ' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] = 'Phiên bản %s';
